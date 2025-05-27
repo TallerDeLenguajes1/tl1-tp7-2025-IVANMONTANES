f69b104c72832ee62340cfd7c61865cdffb35693
ivan <ivanmontanesfilipit@gmail.com>
2025-05-27 13:10:32 -0300

    ejercicio 2 apartado c terminado

--- sisPersonal/Program.cs
+++ sisPersonal/Program.cs
@@ -1,7 +1,5 @@
 ﻿using EspacioEmpleado;
-
-Empleado empleado = new Empleado("ivan", "montañes", new DateTime(2005,1, 27), 'c', new DateTime(2010,4, 25), 650000, Cargos.Ingeniero);
-Console.WriteLine($"antiguedad: {empleado.Antiguedad()}");
-Console.WriteLine($"edad: {empleado.Edad()}");
-Console.WriteLine($"anios para jubilarse: {empleado.AniosParaJubilarse()}");
-Console.WriteLine($"adicional: {empleado.Salario()}");
+Empleado empleado1 = new Empleado("ivan", "montañes", new DateTime(2005, 1, 27), 's', new DateTime(2010, 4, 25), 1450000, Cargos.Auxiliar);
+Empleado empleado2 = new Empleado("santiago", "gonzalez", new DateTime(2005, 2, 14), 's', new DateTime(2022, 5, 5), 1200000, Cargos.Ingeniero);
+Empleado empleado3 = new Empleado("fran", "graib", new DateTime(2004, 4, 28), 'c', new DateTime(2021, 4, 15), 1000000, Cargos.Administrativo);
+Empleado[] empleados = {empleado1,empleado2,empleado3};
